fad40927f3e94669a2731a4982cc9e80cbf401c8
Erin Raif <eraif1999@gmail.com>
2021-12-09 22:19:33 +0000

    part 2 solution

--- day09.py
+++ day09.py
@@ -6,45 +6,75 @@ Created on Thu Dec  9 09:29:58 2021
 """
 
 import numpy as np
-from scipy.ndimage.filters import minimum_filter
+from collections import Counter
+
+class coords:
+    def __init__(self, x_in, y_in):
+        self.x = x_in
+        self.y = y_in
+    def __repr__(self):
+        return "(" + str(self.x) + ", " + str(self.y) +")"
+    def __eq__(self, o):
+        # Could also do this using key
+        return (self.x == o.x and self.y == o.y)
+    def __key(self):
+        return (self.x, self.y)
+    def __hash__(self):
+        # Make hashable for use with counter
+        return hash(self.__key())
+    
+
+def find_minima(j,i,levels):
+    """ Recursively find co-ordinates for the minima a particular point leads to.
+    Note lots of logic holes - relies on the unique formulation of the problem such that
+    there are never places where all neighbours are of equal height, and there is only
+    one minimum per basin."""
+    neighbours = [levels[j][i+1], levels[j][i-1], levels[j-1][i], levels[j+1][i]]
+    if min(neighbours) > levels[j][i]:
+        return j, i, levels
+    else:
+        if levels[j][i+1] < levels[j][i]:
+            return find_minima(j, i+1, levels)
+        elif levels[j][i-1] < levels[j][i]:
+            return find_minima(j, i-1, levels)
+        elif levels[j+1][i] < levels[j][i]:
+            return find_minima(j+1, i, levels)    
+        elif levels[j-1][i] < levels[j][i]:
+            return find_minima(j-1, i, levels)     
+
 data = open('day09.txt').read().split()
 
-# data = """2199943210
-# 3987894921
-# 9856789892
-# 8767896789
-# 9899965678""".split()
 levels = []
 for s in data:
     levels.append([])
     for i in s:
         levels[-1].append(int(i))
+# Add border of 9s to simplify logic.
 for row in levels:
     row.insert(0,9)
     row.append(9)
 levels.insert(0,[9]*len(levels[0]))
 levels.append([9]*len(levels[0]))
-# print(np.array(levels))
 
-risky = []        
+risks = []        
 rows = len(levels)
 cols = len(levels[0])
-
+# Part 1 - find the sum of risk values (heights of minima + 1)
 for j in range(1,rows-1):
     for i in range(1,cols-1):
         neighbours = [levels[j][i+1], levels[j][i-1], levels[j-1][i], levels[j+1][i]]
         if min(neighbours) > levels[j][i]:
-            risky.append(levels[j][i])        
-
-print(np.sum(risky)+len(risky))
+            risks.append(levels[j][i] +1)     
+print("Part 1:", np.sum(risks))
+# Part 2 - find the sizes of the three largest basins and multiply them
+minima = []
+for j in range(1, rows-1):
+    for i in range(1, cols-1):
+        if levels[j][i] != 9:
+            y, x, _ = find_minima(j,i,levels)
+            minima.append(coords(x,y))
+# Find most-commonly occuring minima: no. occurrences = basin size
+count = Counter(minima)
+print("Part 2:", count.most_common(3)[0][1] * count.most_common(3)[1][1] * count.most_common(3)[2][1])
             
     
-        
-        
-# levels = np.array(levels)
-# minima = (levels == minimum_filter(levels,sizemode='constant',cval=10.0))
-# res = np.where(1 == minima)
-# risk_sum = 0
-# for i in range(len(res[0])):
-#     risk_sum += levels[res[0][i],res[1][i]] + 1
-# print(risk_sum)
